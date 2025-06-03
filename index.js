08722952069757af2a8f579aaac7477cae614d58
gyunam-bark <gyunam.bark@gmail.com>
2025-06-03 15:37:27 +0900

    feat: initilize npm project and set eslint, prettier

---
+++ index.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const main = () => {
+  console.log(
+    '!@#!@!@!@SDAHsahdjkhaskjdhgaskjd' +
+      'asdasdasdawdhaosdawiod' +
+      'adhajksdhkjashdkjad'
+  );
+};
