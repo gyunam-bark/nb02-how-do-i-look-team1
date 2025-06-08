597429fcbaddf2569c68438589ccf6ef9781eb0d
hsjun <hippo8976@gmail.com>
2025-06-08 21:15:59 +0900

    fix: 댓글 리스폰스 수정

--- src/controllers/comment-controllers.js
+++ src/controllers/comment-controllers.js
@@ -5,12 +5,10 @@ export const createComment = async (req, res, next) => {
     const comment = await createCommentService(req);
 
     return res.status(200).json({
-      comment: {
-        commentId: comment.commentId,
-        nickname: comment.nickname,
-        content: comment.content,
-        createdAt: comment.createdAt,
-      },
+      id: comment.commentId,
+      nickname: comment.nickname,
+      content: comment.content,
+      createdAt: comment.createdAt,
     });
   } catch (err) {
     next(err);
