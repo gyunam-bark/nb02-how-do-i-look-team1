ca613bbc672cff1b8c5a437b6ddfb82da0b73e04
gyunam-bark <gyunam.bark@gmail.com>
2025-06-10 14:31:10 +0900

    feat: Curation TEST 추가

--- public/app.js
+++ public/app.js
@@ -5,6 +5,7 @@ const app = document.getElementById('app');
 const main = async () => {
   app.appendChild(await routerContainer('./json/style-routes.json'));
   app.appendChild(await routerContainer('./json/ranking-routes.json'));
+  app.appendChild(await routerContainer('./json/curation-routes.json'));
   app.appendChild(await routerContainer('./json/image-routes.json'));
   app.appendChild(await routerContainer('./json/tag-routes.json'));
 };
