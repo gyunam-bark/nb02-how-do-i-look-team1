737a4ed33472c59ecbd9877b0611324ea05ddf0c
gyunam-bark <gyunam.bark@gmail.com>
2025-06-10 09:26:40 +0900

    fix: totalCount->totalItemCount 수정, tagId->id 로 수정

--- src/services/tag-services.js
+++ src/services/tag-services.js
@@ -4,11 +4,17 @@ export const getTagList = async () => {
   // 태그 목록
   const tagList = await db.tag.findMany();
 
+  // tagId -> id 로 변환
+  const formattedTagList = tagList.map(({ tagId, ...other }) => ({
+    id: tagId,
+    ...other,
+  }));
+
   // 태그 목록의 총 갯수
-  const totalCount = await db.tag.count();
+  const totalItemCount = await db.tag.count();
 
   return {
-    totalCount: totalCount,
-    list: tagList,
+    totalItemCount: totalItemCount,
+    data: formattedTagList,
   };
 };
