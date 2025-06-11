53b8322b4a80bb1c7dd0cc291f66fa5ca0071a8a
권나현 <kwonnahyun0125@gmail.com>
2025-06-11 17:56:37 +0900

    fix: style 라우터에 DTO 미들웨어 적용(수정)

--- src/routes/style-routes.js
+++ src/routes/style-routes.js
@@ -1,12 +1,21 @@
 import { Router } from 'express';
 import { StyleController } from '../controllers/style-controllers.js';
+import {
+  validateRequest,
+  createStyleSchema,
+  getStyleListSchema,
+  getStyleDetailSchema,
+  updateStyleSchema,
+  deleteStyleSchema
+} from '../middlewares/dto-middleware.js';
 
 const router = Router();
 
-router.post('/', StyleController.createStyle); // 스타일 등록
-router.get('/', StyleController.getStyles); // 스타일 목록
-router.get('/:styleId', StyleController.getStyleDetail); // 스타일 상세
-router.put('/:styleId', StyleController.updateStyle); // 스타일 수정
-router.delete('/:styleId', StyleController.deleteStyle); // 스타일 삭제 
+router.post('/', validateRequest(createStyleSchema), StyleController.createStyle);                 // 등록
+router.get('/', validateRequest(getStyleListSchema), StyleController.getStyles);                  // 목록
+router.get('/:styleId', validateRequest(getStyleDetailSchema), StyleController.getStyleDetail);   // 상세
+router.put('/:styleId', validateRequest(updateStyleSchema), StyleController.updateStyle);          // 수정
+router.delete('/:styleId', validateRequest(deleteStyleSchema), StyleController.deleteStyle);       // 삭제
 
 export default router;
+
