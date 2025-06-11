b05260b4ccbd34fb0098cb0e777f0e5462370bc5
권나현 <kwonnahyun0125@gmail.com>
2025-06-11 22:25:58 +0900

    docs: style-controllers 함수명 수정

--- src/routes/style-routes.js
+++ src/routes/style-routes.js
@@ -12,7 +12,7 @@ import {
 const router = Router();
 
 router.post('/', validateRequest(createStyleSchema), StyleController.createStyle);                 // 등록
-router.get('/', validateRequest(getStyleListSchema), StyleController.getStyles);                  // 목록
+router.get('/', validateRequest(getStyleListSchema), StyleController.getStyleList);                  // 목록
 router.get('/:styleId', validateRequest(getStyleDetailSchema), StyleController.getStyleDetail);   // 상세
 router.put('/:styleId', validateRequest(updateStyleSchema), StyleController.updateStyle);          // 수정
 router.delete('/:styleId', validateRequest(deleteStyleSchema), StyleController.deleteStyle);       // 삭제
