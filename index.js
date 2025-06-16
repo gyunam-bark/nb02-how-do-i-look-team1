3e077bca684bf2291a198ef8bb0a1a5a7d521e52
gyunam-bark <gyunam.bark@gmail.com>
2025-06-16 21:44:46 +0900

    feat: /docs 스웨거 엔드포인트 추가

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-
 // 서버 실행
 
 import dotenv from 'dotenv';
 import Server from './src/server.js';
+import { readSwaggerJson } from './src/middlewares/swagger-middleware.js';
 
 const main = async () => {
   dotenv.config();
+
+  await readSwaggerJson();
 
   const server = new Server();
   server.run();
--- src/routes/root-routes.js
+++ src/routes/root-routes.js
@@ -1,19 +1,8 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import RootController from '../controllers/root-controller.js';
 
 const rootRouter = express.Router();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-rootRouter.use(express.static(path.join(__dirname, '../../public')));
-
-rootRouter
-  .route('/')
-  // API 테스터
-  .get((req, res) => {
-    res.sendFile(path.join(__dirname, '../../public', 'index.html'));
-  });
+rootRouter.get('/', RootController.handleHealthCheck);
 
 export default rootRouter;
