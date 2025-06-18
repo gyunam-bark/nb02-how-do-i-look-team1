8228d9dddb501750930972ef8c16b60df363ae32
JinsolKim <jinsol032360@gmail.com>
2025-06-18 16:42:01 +0900

    refactor: 랭킹 목록 점수 계산 및 정렬 로직을 서비스에서 컨트롤러로 이동하여 책임 분리, 불필요한 주석 제거

--- src/controllers/image-controller.js
+++ src/controllers/image-controller.js
@@ -17,51 +17,6 @@ class ImageUploadController {
         return res.status(400).json({ error: '이미지 파일이 필요합니다.' });
       }
 
-      // const uploadedImages = [];
-
-      // for (const file of files) {
-      //   const { originalname, path: tempPath, mimetype } = file;
-
-      //   // 원래 이름과 확장자 분리
-      //   const ext = path.extname(originalname);
-      //   const baseName = path.basename(originalname, ext);
-
-      //   // 공백, 특수문자 제거 (파일 이름으로 안전하게 만들기)
-      //   const safeBaseName = baseName.replace(/[^a-zA-Z0-9_-]/g, '_');
-
-      //   // 중복 방지위해 timestamp 추가
-      //   const timestamp = Date.now();
-      //   const firebaseFileName = `${safeBaseName}_${timestamp}${ext}`;
-      //   const destination = `images/${firebaseFileName}`; // Firebase 내 경로
-
-      //   // Firebase에 업로드
-      //   await bucket.upload(tempPath, {
-      //     destination,
-      //     metadata: {
-      //       contentType: file.mimetype,
-      //     },
-      //   });
-
-      //   // 임시 파일 삭제
-      //   fs.unlinkSync(tempPath);
-
-      //   // 공개 URL 생성
-      //   const fileRef = bucket.file(destination);
-      //   const [url] = await fileRef.getSignedUrl({
-      //     action: 'read',
-      //     expires: '03-01-2500',
-      //   });
-
-      //   // DB 저장
-      //   const uploaded = await this.imageService.createImage({
-      //     imageUrl: url,
-      //   });
-
-      //   uploadedImages.push(uploaded.imageUrl);
-      // }
-
-      // 각 파일마다 하나의 응답을 return 하도록 수정
-
       const { originalname, path: tempPath, mimetype } = file;
       const ext = path.extname(originalname);
       const baseName = path.basename(originalname, ext);
